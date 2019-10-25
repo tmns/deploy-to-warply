09abc921b2ca912b79f886c889bad5a47caaf96b
methias <tmn5@pm.me>
2019-10-25 10:36:48 +0300

    change config variables to include remote dir

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class Config:
     remote_port = environ.get('REMOTE_PORT')
     remote_user = environ.get('REMOTE_USER')
     remote_password = environ.get('REMOTE_PASSWORD')
-    remote_dir = environ.get('REMOTE_DIR')
+    remote_upload_dir = environ.get('REMOTE_UPLOAD_DIR')
     local_dir = environ.get('LOCAL_DIR')
