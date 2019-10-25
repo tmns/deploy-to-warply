7c59ac26dcc8375a7cbce6685ac6f111960e12ff
methias <tmn5@pm.me>
2019-10-25 16:08:03 +0300

    add default values for port & remote upload dir

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ load_dotenv(verbose=True)
 
 class Config:
     remote_server = environ.get('REMOTE_SERVER')
-    remote_port = environ.get('REMOTE_PORT')
+    remote_port = environ.get('REMOTE_PORT') or 22
     remote_user = environ.get('REMOTE_USER')
     remote_password = environ.get('REMOTE_PASSWORD')
-    remote_upload_dir = environ.get('REMOTE_UPLOAD_DIR')
+    remote_upload_dir = environ.get('REMOTE_UPLOAD_DIR') or '/tmp'
     remote_final_dir = environ.get('REMOTE_FINAL_DIR')
     local_dir = environ.get('LOCAL_DIR')
