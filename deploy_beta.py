f78545b693f80fc9b04b577a07f67930eb2328a7
methias <tmn5@pm.me>
2019-10-31 12:01:44 +0200

    improve formatting

--- deploy_beta.py
+++ deploy_beta.py
@@ -5,14 +5,16 @@ from os import environ
 from dotenv import load_dotenv
 from paramiko import SSHClient, AutoAddPolicy, RSAKey
 from paramiko.auth_handler import AuthenticationException
-from scp import SCPClient, SCPException 
+from scp import SCPClient, SCPException
 from io import StringIO
 
 try:
     load_dotenv(verbose=True)
 except Exception as ex:
-    print(f'encountered the following error when attempting to load .env:\n{ex}')
+    print(
+        f'encountered the following error when attempting to load .env:\n{ex}')
     sys.exit()
+
 
 class Config:
     remote_server = environ.get('REMOTE_SERVER')
@@ -21,7 +23,7 @@ class Config:
     remote_password = environ.get('REMOTE_PASSWORD')
     remote_upload_dir = environ.get('REMOTE_UPLOAD_DIR') or '/tmp'
     remote_final_dir = environ.get('REMOTE_FINAL_DIR')
-    local_dir = environ.get('LOCAL_DIR')  or '/dist'
+    local_dir = environ.get('LOCAL_DIR') or '/dist'
 
 
 class Client:
@@ -39,7 +41,8 @@ class Client:
                 client = SSHClient()
                 client.load_system_host_keys()
                 client.set_missing_host_key_policy(AutoAddPolicy())
-                client.connect(self.remote_server, self.remote_port, self.remote_user, self.remote_password)
+                client.connect(self.remote_server, self.remote_port,
+                               self.remote_user, self.remote_password)
             except Exception as ex:
                 print(ex)
             finally:
@@ -62,7 +65,8 @@ class Client:
         """Upload a single file to a remote directory."""
         if self.client is None:
             self.client = self.__connect()
-        print(f'uploading {local_dir} to {Config.remote_server}:{remote_upload_dir}...')
+        print(
+            f'uploading {local_dir} to {Config.remote_server}:{remote_upload_dir}...')
         scp = SCPClient(self.client.get_transport(), progress=self.__progress)
         try:
             scp.put(local_dir,
@@ -75,17 +79,20 @@ class Client:
 
     def __progress(self, filename, size, sent):
         """Display SCP progress."""
-        sys.stdout.write(f'uploading {filename}: {float(sent) / float(size) * 100:.2f}%    \r')
+        sys.stdout.write(
+            f'uploading {filename}: {float(sent) / float(size) * 100:.2f}%    \r')
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="deploy an ember project to beta")
-    parser.add_argument("-b", "--build", help="calls 'ember b' to build the current ember project", action="store_true")
+    parser = argparse.ArgumentParser(
+        description="deploy an ember project to beta")
+    parser.add_argument(
+        "-b", "--build", help="calls 'ember b' to build the current ember project", action="store_true")
     args = parser.parse_args()
     if args.build:
         print('building project...')
         try:
-            subprocess.run(["ember","b"], check=True)
+            subprocess.run(["ember", "b"], check=True)
         except Exception as ex:
             sys.exit()
 
@@ -94,12 +101,12 @@ if __name__ == '__main__':
     remote_final_dir = Config.remote_final_dir
 
     client = Client(Config)
-    
+
     client.upload(local_dir, remote_upload_dir)
     client.execute(f'rm -rf {remote_final_dir}/{local_dir}.bak')
-    client.execute(f'mv {remote_final_dir}/{local_dir} {remote_final_dir}/{local_dir}.bak')
+    client.execute(
+        f'mv {remote_final_dir}/{local_dir} {remote_final_dir}/{local_dir}.bak')
     client.execute(f'mv {remote_upload_dir}/{local_dir} {remote_final_dir}')
-    
+
     client.disconnect()
     print('finished!')
-
