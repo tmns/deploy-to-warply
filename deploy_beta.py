7c720814f667060efa55244030c91ed5367769b2
methias <tmn5@pm.me>
2019-10-31 13:35:26 +0200

    add option to define custom env file and add init method to Config class

--- deploy_beta.py
+++ deploy_beta.py
@@ -1,29 +1,28 @@
-import sys
-import subprocess
-import argparse
+import os, sys, subprocess, argparse
 from os import environ
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv
 from paramiko import SSHClient, AutoAddPolicy, RSAKey
 from paramiko.auth_handler import AuthenticationException
 from scp import SCPClient, SCPException
 from io import StringIO
 
-try:
-    load_dotenv(dotenv_path=".env", verbose=True)
-except Exception as ex:
-    print(
-        f'encountered the following error when attempting to load .env:\n{ex}')
-    sys.exit()
-
 
 class Config:
-    remote_server = environ.get('REMOTE_SERVER')
-    remote_port = environ.get('REMOTE_PORT') or 22
-    remote_user = environ.get('REMOTE_USER')
-    remote_password = environ.get('REMOTE_PASSWORD')
-    remote_upload_dir = environ.get('REMOTE_UPLOAD_DIR') or '/tmp'
-    remote_final_dir = environ.get('REMOTE_FINAL_DIR')
-    local_dir = environ.get('LOCAL_DIR') or 'dist'
+    def __init__(self, env):
+        print(f'loading environment file "{env}"...')
+        if not os.path.exists(env):
+            print(
+                f'given environment file "{env}" does not exist\nexiting...')
+            sys.exit()
+        load_dotenv(dotenv_path=env, verbose=True)
+        
+        self.remote_server = environ.get('REMOTE_SERVER')
+        self.remote_port = environ.get('REMOTE_PORT') or 22
+        self.remote_user = environ.get('REMOTE_USER')
+        self.remote_password = environ.get('REMOTE_PASSWORD')
+        self.remote_upload_dir = environ.get('REMOTE_UPLOAD_DIR') or '/tmp'
+        self.remote_final_dir = environ.get('REMOTE_FINAL_DIR')
+        self.local_dir = environ.get('LOCAL_DIR') or 'dist'
 
 
 class Client:
@@ -65,8 +64,6 @@ class Client:
         """Upload a single file to a remote directory."""
         if self.client is None:
             self.client = self.__connect()
-        print(
-            f'uploading {local_dir} to {Config.remote_server}:{remote_upload_dir}...')
         scp = SCPClient(self.client.get_transport(), progress=self.__progress)
         try:
             scp.put(local_dir,
@@ -87,8 +84,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="deploy an ember project to beta")
     parser.add_argument(
+        "-e", "--env", help="sets environment file to given file, defaults to .env", default=".env")
+    parser.add_argument(
         "-b", "--build", help="calls 'ember b' to build the current ember project", action="store_true")
     args = parser.parse_args()
+
+    config = Config(args.env)
+
     if args.build:
         print('building project...')
         try:
@@ -96,11 +98,11 @@ if __name__ == '__main__':
         except Exception as ex:
             sys.exit()
 
-    local_dir = Config.local_dir
-    remote_upload_dir = Config.remote_upload_dir
-    remote_final_dir = Config.remote_final_dir
+    local_dir = config.local_dir
+    remote_upload_dir = config.remote_upload_dir
+    remote_final_dir = config.remote_final_dir
 
-    client = Client(Config)
+    client = Client(config)
 
     client.upload(local_dir, remote_upload_dir)
     client.execute(f'rm -rf {remote_final_dir}/{local_dir}.bak')
